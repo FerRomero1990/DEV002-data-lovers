d26767c7dab031859abb86ef6de3e4a2a60243b9
katterin olivares <katterinolivaresalcalde28@gmail.com>
2022-11-23 13:05:25 -0500

    se trajo las imagenes y nombres a html

--- src/data.js
+++ src/data.js
@@ -1,9 +1,27 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+export const peticion = async () =>{
+  const link = 'data/lol/lol.json';
+  const response = await fetch(link)
+  const datos = await response.json()
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+  //console.log(datos)
+
+  const contenedor = document.getElementById("container")
+  const fragment = document.createDocumentFragment()
+
+  const arrayContainer = Object.values(datos)
+  const personajes = arrayContainer[3]
+  console.log(arrayContainer)
+
+  for (const item of Object.values(personajes)) {
+    const nameChamp = document.createElement("p")
+    const image = document.createElement("img")
+    image.src = item.splash
+    nameChamp.textContent = item.name
+    fragment.append(nameChamp,image)
+    contenedor.append(fragment)
+    console.log(item.name)
+  }
+
+}
