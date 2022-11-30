2c6cc7d41fef33f2c7f654a6a5c6056aba5274f3
katterin olivares <katterinolivaresalcalde28@gmail.com>
2022-11-30 17:33:31 -0500

    tarjetas con img,nombre y titulo

--- src/data.js
+++ src/data.js
@@ -1,38 +1,63 @@
-//creamos una funcion asincrona y exportamos en un archivo principal
+// //creamos una funcion asincrona y exportamos en un archivo principal
+// export const peticion = async () =>{
+//   //crear una variable con la direccion del archivo json
+//   const link = 'data/lol/lol.json';
+//   //usamos fetch para llamar al enlace
+//   const response = await fetch(link)
+//   //el await espera una respuesta (promesa pendiente)
+//   //con json transformamos la respuesta en objeto
+//   const datos = await response.json()
+//   //console.log(datos)
+
+//   //se crea variable contenedor donde llamamos al div container en html
+//   const contenedor = document.getElementById("container")
+//   //en el fragment contiene las etiquetas que estamos creando dinamicamente que almacenan
+//   const fragment = document.createDocumentFragment()
+//   //creamos un array convirtiendo un objeto con values
+//   const arrayContainer = Object.values(datos)
+//   //crear variable para guardar la posicion 3
+//   const personajes = arrayContainer[3]
+//   console.log(arrayContainer)
+
+//   //se usa de nuevo object.values para convertir el objeto en array
+//   for (const item of Object.values(personajes)) {
+//     //se crea las etiquetas dinamicas
+//     const nameChamp = document.createElement("p")
+//     const image = document.createElement("img")
+
+//     //const nameChamp = document.getElementById("name")
+//     //const image = document.getElementById("champion")
+
+//     //al atributo src le colocamos el enlace
+//     image.src = item.splash
+//     //al textContent le colocamos los nombres
+//     nameChamp.textContent = item.name
+//     //accedemos al fragment y le hacemos un append
+//     fragment.append(nameChamp,image)
+//     //dentro de un contenedor metemos todas las etiquetas creadas
+//     contenedor.append(fragment)
+//     console.log(item.name)
+//   }
+
+// }
+
 export const peticion = async () =>{
-  //crear una variable con la direccion del archivo json
   const link = 'data/lol/lol.json';
-  //usamos fetch para llamar al enlace
   const response = await fetch(link)
-  //el await espera una respuesta (promesa pendiente)
-  //con json transformamos la respuesta en objeto
   const datos = await response.json()
-  //console.log(datos)
 
-  //se crea variable contenedor donde llamamos al div container en html
-  const contenedor = document.getElementById("container")
-  //en el fragment contiene las etiquetas que estamos creando dinamicamente que almacenan
+  const contenedor = document.getElementById("gallery")
   const fragment = document.createDocumentFragment()
-  //creamos un array convirtiendo un objeto con values
-  const arrayContainer = Object.values(datos)
-  //crear variable para guardar la posicion 3
-  const personajes = arrayContainer[3]
-  console.log(arrayContainer)
+  const templateGallery = document.getElementById('template-gallery').content;
 
-  //se usa de nuevo object.values para converir el objeto en array
-  for (const item of Object.values(personajes)) {
-    //se crea las etiquetas dinamicas
-    const nameChamp = document.createElement("p")
-    const image = document.createElement("img")
-    //al atributo src le colocamos el enlace
-    image.src = item.splash
-    //al textContent le colocamos los nombres
-    nameChamp.textContent = item.name
-    //accedemos al fragment y le hacemos un append
-    fragment.append(nameChamp,image)
-    //dentro de un contenedor metemos todas las etiquetas creadas
-    contenedor.append(fragment)
-    console.log(item.name)
+  for (const item of Object.values(datos.data)) {
+    templateGallery.querySelector('.gallery__img').src = item.splash
+    templateGallery.querySelector('.gallery__name').textContent=item.name
+    templateGallery.querySelector('.gallery__surname').textContent=item.title
+
+    const clone = templateGallery.cloneNode(true)
+    fragment.append(clone)
   }
+  contenedor.append(fragment)
 
 }
