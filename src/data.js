604a048718b2aac4a6d93767e2a39b2fa2818ac8
FerRomero1990 <aidafernanda1990@hotmail.com>
2022-11-30 23:16:18 -0500

    agregando codigo salvado

--- src/data.js
+++ src/data.js
@@ -1,1 +1,19 @@
-
+export const peticion = async () =>{
+    const link = 'data/lol/lol.json';
+    const response = await fetch(link)
+    const datos = await response.json()
+  
+    const contenedor = document.getElementById("gallery")
+    const fragment = document.createDocumentFragment()
+    const templateGallery = document.getElementById('template-gallery').content;
+  
+    for (const item of Object.values(datos.data)) {
+      templateGallery.querySelector('.gallery__img').src = item.splash
+      templateGallery.querySelector('.gallery__name').textContent=item.name
+      templateGallery.querySelector('.gallery__surname').textContent=item.title
+  
+      const clone = templateGallery.cloneNode(true)
+      fragment.append(clone)
+    }
+    contenedor.append(fragment)
+  }
