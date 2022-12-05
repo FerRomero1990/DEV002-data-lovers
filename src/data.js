05b6a764c384f5c6e71ad23e825a7fc8b5b2401b
FerRomero1990 <aidafernanda1990@hotmail.com>
2022-12-05 15:57:59 -0500

    ok giro de cards

--- src/data.js
+++ src/data.js
@@ -10,11 +10,12 @@ export const peticion = async () =>{
     for (const item of Object.values(datos.data)) {
       templateGallery.querySelector('.gallery__img').src = item.splash
       templateGallery.querySelector('.gallery__name').textContent=item.name
-      templateGallery.querySelector('.gallery__surname').textContent=item.title
-      templateGallery.querySelector('.gallery__attack').textContent=('Ataque ' + (item.info.attack))
-      templateGallery.querySelector('.gallery__defense').textContent=('Defensa ' + (item.info.defense))
-      templateGallery.querySelector('.gallery__magic').textContent=('Magia ' + (item.info.magic))
-      templateGallery.querySelector('.gallery__difficulty').textContent=('Dificultad ' + (item.info.difficulty))
+      templateGallery.querySelector('.gallery__surname').textContent=item.title.toUpperCase()
+      templateGallery.querySelector('.gallery__attack').textContent=('Attack: ' + (item.info.attack))
+      templateGallery.querySelector('.gallery__defense').textContent=('Defense: ' + (item.info.defense))
+      templateGallery.querySelector('.gallery__magic').textContent=('Magic: ' + (item.info.magic))
+      templateGallery.querySelector('.gallery__difficulty').textContent=('Difficulty: ' + (item.info.difficulty))
+      templateGallery.querySelector('.gallery__tags').textContent=item.tags
   
       const clone = templateGallery.cloneNode(true)
       fragment.append(clone)
