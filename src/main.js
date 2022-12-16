5799506f65c29b68610a8beef6552f0fc92afb64
katterin olivares <katterinolivaresalcalde28@gmail.com>
2022-12-16 09:49:47 -0500

    eliminado de coemntarios

--- src/main.js
+++ src/main.js
@@ -5,11 +5,3 @@ import { showMenu } from "./menu.js";
 peticion(champDat[0].data)
 showMenu()
 
-/*import { champDat } from "./data/lol/lol.js";
-console.log(champDat);*/
-
-/*import { filtChampFighter } from "./data/lol/lol.js";
-
-filtChampFighter()
-
-console.log(filtChampFighter);*/
