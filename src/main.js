1f5297210d1605f50783936a5498921c4465053b
FerRomero1990 <aidafernanda1990@hotmail.com>
2022-12-16 11:31:44 -0500

    recuperando codigo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
+
 import { peticion } from "./data.js";
-import { champDat } from './data/lol/lol.js'
-import { showMenu } from "./menu.js";
-peticion(champDat[0].data)
-showMenu()
 
+//peticion()
 
+/*document.getElementById("btnfighter").addEventListener("click", function (){
+    
+})*/
 
